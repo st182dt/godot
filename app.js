151d6ecb6dc771a3a1e2457e54bebb87ca7a66f0
st182dt <58052770+st182dt@users.noreply.github.com>
2024-02-05 10:36:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,57 +12,19 @@ console.log("Server started.");
  
 var SOCKET_LIST = {};
 var PLAYER_LIST = {};
-var NPC_LIST = {};
-var TILES = [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
-
-var Npc = function(id){
-	var self = {
-		x:250,
-		y:250,
-		id:id,
-	}
-	self.updatePosition = function(){
-		self.x += 10;
-		if (self.x > 500) {
-			self.x = 250;
-		}
-	}
-	return self;
-}
-
-var npc_id = Math.random();
-var npc = Npc(npc_id);
-NPC_LIST[npc_id] = npc;
 
 var Player = function(id){
 	var self = {
 		x:250,
 		y:250,
-		id:id,
-		number:"" + Math.floor(10 * Math.random()),
-		pressingRight:false,
-		pressingLeft:false,
-		pressingUp:false,
-		pressingDown:false,	
-		maxSpd:80,
-		time:0,
-	}
-	self.updatePosition = function(){
-		if(self.pressingRight)
-			self.x += self.maxSpd;
-		if(self.pressingLeft)
-			self.x -= self.maxSpd;
-		if(self.pressingUp)
-			self.y -= self.maxSpd;
-		if(self.pressingDown)
-			self.y += self.maxSpd;
+		id:id
 	}
 	return self;
 }
  
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
-	
+
 	socket.id = Math.random();
 	for(var i in SOCKET_LIST){
 		var socket2 = SOCKET_LIST[i];
@@ -80,17 +42,7 @@ io.sockets.on('connection', function(socket){
 		});
 	}
 	socket.emit('allPlayers',{positions:players});
-	var npcs = [];
-	for(var i in NPC_LIST){
-		var npc = NPC_LIST[i];
-		npcs.push({
-			x:npc.x,
-			y:npc.y,
-			id:npc.id
-		});
-	}
-	socket.emit('allNpcs',{positions:npcs});
- 
+
 	var player = Player(socket.id);
 	PLAYER_LIST[socket.id] = player;
  
@@ -99,63 +51,25 @@ io.sockets.on('connection', function(socket){
 		delete PLAYER_LIST[socket.id];
 	});
 
-	socket.on('getMap',function(){
-		socket.emit('newMap',TILES);
-	});
- 
-	socket.on('keyPress',function(data){
-		if(data.inputId === 'left')
-			player.pressingLeft = data.state;
-		else if(data.inputId === 'right')
-			player.pressingRight = data.state;
-		else if(data.inputId === 'up')
-			player.pressingUp = data.state;
-		else if(data.inputId === 'down')
-			player.pressingDown = data.state;
-	});
-
 	socket.on('newPos',function(data){
 		player.x = data.x;
 		player.y = data.y;
-		player.time = data.time;
 	});
 
-	socket.on('putTile',function(data){
-		TILES[data.tile] = data.color;
-	});
- 
- 
 });
  
 setInterval(function(){
 	var pack = [];
 	for(var i in PLAYER_LIST){
 		var player = PLAYER_LIST[i];
-		player.updatePosition();
 		pack.push({
 			x:player.x,
 			y:player.y,
-			id:player.id,
-			time:player.time,
-			number:player.number
-		});
-	}
-	var pack2 = [];
-	for(var i in NPC_LIST){
-		var npc = NPC_LIST[i];
-		npc.updatePosition();
-		pack2.push({
-			x:npc.x,
-			y:npc.y,
-			id:npc.id
+			id:player.id
 		});
 	}
 	for(var i in SOCKET_LIST){
 		var socket = SOCKET_LIST[i];
-		socket.emit('newPositions',{positions:pack,positionsnpc:pack2});
+		socket.emit('newPositions',{positions:pack});
 	}
- 
- 
- 
- 
-},1000/30);
+},1000/10);
